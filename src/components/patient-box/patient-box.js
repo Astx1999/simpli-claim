6229d3640f92a318bd08d9d8ad6d02e5ecdce592
Astx1999 <astxofficial@gmail.com>
2021-08-07 13:37:28 +0400

    font family fix, patient card styles fix

--- src/components/patient-box/patient-box.js
+++ src/components/patient-box/patient-box.js
@@ -6,7 +6,10 @@ const PatientBox = ({name, desc, icon, color}) => {
     return(
         <div className={styles.root}>
             <div className={styles.status} style={{backgroundColor: color}}/>
-            <img className={styles.icon} src={icon} alt=""/>
+            <div className={styles.iconWrapper}>
+                <img className={styles.icon} src={icon} alt=""/>
+            </div>
+
             <div className={styles.info}>
                 <p className={styles.name}>{name}</p>
                 <p className={styles.desc}>{desc}</p>
